52c26d8170b83e5e74c9a61b1a74bd8c555f9e97
Adeola idowu <adeolaidowu27@gmail.com>
2022-05-03 18:06:32 +0100

    lesson-five completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Link  from "next/link";
+import Link from "next/link";
+import { AUTH_TOKEN } from "../constants/constants";
+import Router from "next/router";
 
 const Header = () => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
@@ -12,9 +15,34 @@ const Header = () => {
           new
         </Link>
         <div className="ml1">|</div>
-        <Link href="/create" className="ml1 no-underline black">
-          submit
+        <Link href="/search" className="ml1 no-underline black">
+          search
         </Link>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link href="/create" className="ml1 no-underline black">
+              submit
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              Router.push(`/`);
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link href="/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
       </div>
     </div>
   );
